5e8fae35e502a1ea4fcf18c54b38ad415b063f3a
Stephen Reilly <stephen.p.reilly@gmail.com>
2026-03-29 13:51:44 +0200

    Update 1-positive_or_negative.py

--- cyber-assessment/1-positive_or_negative.py
+++ cyber-assessment/1-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number < 0 :
-    print("is positive")
-elif number > 0 :
-    print("is nergative")
+if number > 0 :
+    print(number, "is positive")
+elif number < 0 :
+    print(number, "is negative")
 else:
-    print("is zero")
+    print(number, "is zero")
